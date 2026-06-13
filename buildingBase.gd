d7d4ba83c68d63416f09aed720b00612d8bfdb39
Asher Feinstein <asher.feinstein@gmail.com>
2026-06-12 21:23:55 -0400

    Carrot Gun
    visuals update, plus carrot scene that pulls from building

--- buildingBase.gd
+++ buildingBase.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
 @export var xPos = 99999
+@export var cost = 0
 func _physics_process(delta: float) -> void:
 	if xPos == 99999:
 		_buildPlace()
+	else:
+		$Pivot.look_at(get_global_mouse_position())
+		
+		#all stuff tower does
 
 func _buildPlace() -> void:
 	position = Vector2(get_global_mouse_position().x,0)
