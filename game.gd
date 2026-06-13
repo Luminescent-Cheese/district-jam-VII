afc863cb3fbd19c8e8dfcd5da8c4a3f25d9353db
Scott Feinstein <Scott_Feinstein@yahoo.com>
2026-06-12 23:56:30 -0400

    fix cow naming
    Ufo detection was broken since cow names didn't always contain "Cow".  Now they do.

--- game.gd
+++ game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 const COW_SCENE = preload("res://cow.tscn")
 const UFO_SCENE = preload("res://ufo.tscn")
+
 func _ready() -> void:
 	spawn_cows(5)
 	spawn_ufo()
@@ -11,8 +12,9 @@ func spawn_cows(num_cows: int) -> void:
 	
 	for i in range(num_cows):	
 		var new_cow = COW_SCENE.instantiate()
-		var cow_location = Vector2(CoordsUtils.get_random_x_in_world(self), 200)
+		var cow_location = Vector2(CoordsUtils.get_random_x_in_world(self), world_bounds.max_y - 120)
 		new_cow.position = cow_location
+		new_cow.name = "Cow %s" % i
 		add_child(new_cow)
 
 func spawn_ufo() -> void:
